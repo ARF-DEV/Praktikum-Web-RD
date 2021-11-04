32c7239f15aa27da8006a35e9ffd831f965a9af4
PopoSnek <ariefuddinsatriadharma@gmail.com>
2021-11-05 00:05:33 +0700

    done

---
+++ delete.php
@@ -0,0 +1,8 @@
+<?php
+include "koneksi.php";
+
+$nim = $_POST["nim"];
+$sql = "DELETE FROM `mahasiswa` WHERE nim = '$nim'";
+$hasil = mysqli_query($kon,$sql);
+
+?>
---
+++ update.php
@@ -0,0 +1,11 @@
+<?php
+include "koneksi.php";
+
+$nim = $_POST["nim"];
+$nama = $_POST["nama"];
+$prodi = $_POST["prodi"];
+$angkatan = $_POST["angkatan"];
+$sql = "update mahasiswa set nama='$nama',prodi='$prodi',angkatan='$angkatan' WHERE nim = '$nim'";
+$hasil = mysqli_query($kon,$sql);
+
+?>
