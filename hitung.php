b92a2ff7596ad1742e1c6c9dac95c7e299bb6ac8
PopoSnek <ariefuddinsatriadharma@gmail.com>
2021-11-14 14:15:16 +0700

    minimum is done (without css, etc)

--- hitung.php
+++ hitung.php
@@ -1,17 +1,33 @@
-<?php
-    $lNum = (int)$_POST["left"];
-    $rNum = (int)$_POST["right"];
-    $hasilTa = $lNum + $rNum;
-    $hasilKu = $lNum - $rNum;
-    $hasilKa = $lNum * $rNum;
-    $hasilBa = $lNum / $rNum;
-    $hasilMo = $lNum % $rNum;
+<?php 
+
+    Class Cart {
+        var int $n_mangga, $n_jambu, $n_salak;
+        var int $harga_mangga = 15000;
+        var int $harga_jambu = 13000;
+        var int $harga_salak = 10000;
+        public function __construct($n_mangga, $n_jambu, $n_salak) {
+            $this->n_mangga = $n_mangga;
+            $this->n_jambu = $n_jambu;
+            $this->n_salak = $n_salak;
+        }
+
+        public function getTotal() {
+            $result = $this->n_jambu * $this->harga_jambu;
+            $result += $this->n_mangga * $this->harga_mangga;
+            $result += $this->n_salak * $this->harga_salak;
+
+            echo $result;
+        }
+    }
 
 
-    ?>
+    $mangga = $_POST['mangga'];
+    $salak = $_POST['salak'];
+    $jambu = $_POST['jambu'];
+
     
-    <h1><?php echo $lNum ?> + <?php echo $rNum ?> = <?php echo $hasilTa ?></h1>
-    <h1><?php echo $lNum ?> - <?php echo $rNum ?> = <?php echo $hasilKu ?></h1>
-    <h1><?php echo $lNum ?> * <?php echo $rNum ?> = <?php echo $hasilKa ?></h1>
-    <h1><?php echo $lNum ?> / <?php echo $rNum ?> = <?php echo $hasilBa ?></h1>
-    <h1><?php echo $lNum ?> % <?php echo $rNum ?> = <?php echo $hasilMo ?></h1>
+    $newCart = new Cart($mangga, $jambu, $salak);
+
+    return $newCart->getTotal();
+
+?>
